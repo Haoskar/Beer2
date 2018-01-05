3c09db2772baa31f7be84d33d38cc3062756464d
RubenTheCube <robertsjob@gmail.com>
2018-01-05 18:27:30 +0100

    kommenterade lite mer

--- search.c
+++ search.c
@@ -15,21 +15,21 @@ int main (void) {
     Vara *start_of_products = products;
     products = Read();
 
-    printf("%i", products->varunummer);
+    //printf("%i", products->varunummer);
     printf("\nVarunummer to search: ");
     fgets(search_word,20, stdin);
     search_word[strlen(search_word) - 1] = '\0';                //removes newline char
 
-    for(int i = 0; i < 16; i++){
-        sprintf(temporary_string, "%d", products->varunummer);  //convert to string
-        printf("\n%s", temporary_string);
+    for(int i = 0; i < 16; i++){                   //ändra 16 till något bättre
+        sprintf(temporary_string, "%d", products->varunummer);  //convert current structs varunummer to string
+        //printf("\n%s", temporary_string);
         if (strcmp(search_word, temporary_string) == 0) {       //compare strings
             printf("\n%s, has the name %s", search_word, products->namn);
             match_found = true;
             break;
         }  
         else
-            products++;
+            products++;     //move over to next struct
     }
     if(!match_found)
         printf("\nThe number %s did not match any varunummer", search_word);

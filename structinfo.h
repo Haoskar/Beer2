668f7f289a9c9e01b4097d4c202fe26d8438e9f6
RubenTheCube <robertsjob@gmail.com>
2018-01-05 13:07:34 +0100

    annan fil för struct

---
+++ structinfo.h
@@ -0,0 +1,12 @@
+struct Vara{
+    int varunummer;
+    char namn[100];
+    float pris;
+    float volym;
+    char typ[100];
+    char stil[100];
+    char forpackning[20];
+    char land[20];
+    char producent[50];
+    float alkoholhalt;
+};
